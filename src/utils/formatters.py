fff565117df4bede0267826c7c66c7645bfb240d
dvilela <dvilelaf@gmail.com>
2025-12-11 12:56:20 +0100

    fix: formatting

--- src/utils/formatters.py
+++ src/utils/formatters.py
@@ -47,8 +47,4 @@ def format_task_es(task) -> str:
     date_str = format_datetime_es(task.deadline)
     priority_str = PRIORITY_MAP.get(task.priority, "⚪")
 
-    title = task.title
-    if title:
-        title = title[0].upper() + title[1:]
-
-    return f"• *{title}* \n  ⏳ {date_str}  |  {priority_str}\n\n"
+    return f"• *{task.title}* \n  ⏳ {date_str}  |  {priority_str}\n\n"
